15b12c6e27b84a631d90e29b45a30444227f7edc
Anupriya Srivastava <anupriya@hawaii.edu>
2015-05-01 21:10:47 -1000

    updated js

--- public/javascripts/bookmarklet.js
+++ public/javascripts/bookmarklet.js
@@ -1,5 +1,43 @@
 /**
  * Created by Anupriya on 5/1/2015.
  */
-(function(){
-    alert("HERE")})();
+(function() {
+    alert("HERE");
+    var httpRequest;
+    url="http://localhost:9000/EnterUrl";
+    //document.getElementById("ajaxButton").onclick = function() { makeRequest('test.html'); };
+
+    function makeRequest(url) {
+        if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+            httpRequest = new XMLHttpRequest();
+        } else if (window.ActiveXObject) { // IE
+            try {
+                httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+            }
+            catch (e) {
+                try {
+                    httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+                }
+                catch (e) {}
+            }
+        }
+
+        if (!httpRequest) {
+            alert('Giving up :( Cannot create an XMLHTTP instance');
+            return false;
+        }
+        httpRequest.onreadystatechange = alertContents;
+        httpRequest.open('GET', url);
+        httpRequest.send("url='+encodeURIComponent(location.href)");
+    }
+
+    function alertContents() {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                alert(httpRequest.responseText);
+            } else {
+                alert('There was a problem with the request.');
+            }
+        }
+    }
+})();
